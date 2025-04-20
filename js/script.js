e9e58ede0b3daaa1bc53806b8f19d8a5ab0e8057
bmohedulislam <bmohedulislam@gmail.com>
2025-04-20 16:55:59 +0600

    index page update

--- js/script.js
+++ js/script.js
@@ -130,4 +130,31 @@ $(function () {
   ]
     });
 
+
+    //Testimonial  part start
+    
+    //Testimonial  part startend
+    //footer counter part start
+    
+    const counters = document.querySelectorAll('.counter-number');
+
+    counters.forEach(counter => {
+      const target = +counter.getAttribute('data-target');
+      let count = 0;
+    
+      const update = () => {
+        const increment = target / 40;
+        if (count < target) {
+          count += increment;
+          counter.innerText = Math.ceil(count);
+          requestAnimationFrame(update);
+        } else {
+          counter.innerText = target;
+        }
+      };
+    
+      update();
+    });
+    //footer counter part end
+
 });
