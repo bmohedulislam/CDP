c9df38f6338a1951eb3393e673e39fd693c51e56
bmohedulislam <bmohedulislam@gmail.com>
2025-04-27 09:51:59 +0600

    new

--- js/script.js
+++ js/script.js
@@ -156,5 +156,16 @@ $(function () {
       update();
     });
     //footer counter part end
+    //menu fix Part start
+    window.addEventListener('scroll', function() {
+      var navbar = document.getElementById('navbar');
+      if (window.scrollY > 0) {
+        navbar.classList.add('fixed');
+      }
+       else {
+        navbar.classList.remove('fixed');
+      }
+    });
+    //menu fix Part end
 
 });
